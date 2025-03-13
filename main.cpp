02cb7cdc631cf998c334e413551ee1a889e2b786
Krishna K R <117531112+krishnakravi@users.noreply.github.com>
2025-03-13 16:26:01 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Lingan guli guli guli gacha lingan goo lingan goo\n";
+  cout << "This is task 5 yo, these are some real big boy numbers  \n";
+  return 0;
+}
